b9e53eefab1e7ecb663596384b23b98f97c33a5a
anumberplate <aluma.kyle@gmail.com>
2025-08-02 10:55:09 +0300

    Cursors and fetiching data

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const app = express()
+const {connectToDb, getDb} = require('./db')
 
-app.listen(3000, ()=>{
-  console.log("App listening on port 3000")
+let db
+connectToDb((err) => {
+  if(!err){
+    app.listen(3000, () => {
+      console.log("App listening on port 3000")
+    })
+    db = getDb()
+    
+  }
+})
+
+//routes
+
+app.get('/books', (req, res) => {
+  let books = []
+  db.collection('books')
+    .find()
+    .sort({author: 1})
+    .forEach(book => books.push(book))
+    .then(() => {
+      res.status(200).json(books)
+    })
+    .catch(() => {
+      res.status(500).json({error: "Could not fetch documents"})
+    })
+
 })
